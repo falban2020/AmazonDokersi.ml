d53bb6e026ff3e0bb668e695d8728d2b133220c1
falban2020 <105378763+falban2020@users.noreply.github.com>
2022-05-11 18:17:40 +0200

    Add files via upload

---
+++ czekaj2.php
@@ -0,0 +1,9 @@
+
+<style>body{background:black;margin:0;padding:0;}</style>
+<body oncontextmenu="if (!window.__cfRLUnblockHandlers) return false; false" data-cf-modified-c1aa4c4a49d04b4829b52afc-="">
+<link href="https://vjs.zencdn.net/7.14.3/video-js.css" rel="stylesheet" />
+<script src="https://vjs.zencdn.net/7.14.3/video.min.js" < script type="text/javascript">
+<script src="https://vjs.zencdn.net/ie8/1.1.2/videojs-ie8.min.js"</script>
+<video id="my-video" class="video-js" controls preload="auto" poster="https://i.imgur.com/7kh2Hux.jpg" data-setup="{}" style="height: 100%; width: 100%;">
+<source src="https://eurodance90.catcast.tv/content/36987/index.m3u8" type="application/vnd.apple.mpegurl">
+</video>
